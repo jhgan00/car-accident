170bf7e71b6a299b71c7b5aecce0fd9945f2406d
--global <--global>
2020-06-13 07:53:08 +0000

    Update clustering

--- Clustering.py
+++ Clustering.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import geopandas as gpd
 import pandas as pd
@@ -19,9 +18,12 @@ data = pd.merge(
     pd.DataFrame({"acdnt_no":gdf.acdnt_no.values, "X":gdf.geometry.x, "Y":gdf.geometry.y}),
     df
 )
+
 del(df)
+
 if not os.path.exists("figs"):
     os.mkdir("figs")
+
 if not os.path.exists("logs"):
     os.mkdir("logs")
 
@@ -34,10 +36,10 @@ for (sido, df) in data.groupby("sido"):
         os.mkdir(f"figs/{sido}")
         print(f"mkdir: figs/{sido}")
     logfile = open(f"logs/{sido}.csv", "a")
-    logfile.write("eps,min_samples,ncluster,mean_cluster_size,outliers\n")
-    prod = product(np.arange(0.002, 0.2, 0.002), np.arange(2,10))
+    logfile.write("eps,min_samples,ncluster,mean-cls_size,std_cls_size,outliers\n")
+    prod = product(np.arange(0.0005, 0.2, 0.0005), np.arange(2,10))
     with tqdm(total=sum([1 for x in prod])) as progress_bar:
-        for (eps, min_samples) in product(np.arange(0.002, 0.2, 0.002), np.arange(2,10)):
+        for (eps, min_samples) in product(np.arange(0.0005, 0.2, 0.0005), np.arange(2,10)):
             eps = round(eps, 4)
             dbscan = DBSCAN(eps=eps, min_samples=min_samples)
             dbscan.fit(std_coords)
@@ -49,13 +51,14 @@ for (sido, df) in data.groupby("sido"):
             if geo.shape[0] == 0:
                 continue
             geo.plot(column="labels", markersize=1.5)
-            mean_cluster_size = int(pd.Series(labels).value_counts().mean())
-
-            record = f"{eps},{min_samples},{ncluster},{mean_cluster_size},{outliers}"
+            stats = pd.Series(dbscan.labels_).value_counts().describe().round(2).values
+            mean, std = stats[1], stats[2]
+            record = f"{eps},{min_samples},{ncluster},{outliers},"+",".join(stats.astype(str))
             logfile.write(
                 f"{record}\n"
             )
-            plt.title(f"eps:{eps}, ms:{min_samples},\nncluster:{ncluster}, mean_cluster_size:{mean_cluster_size}, outliers:{outliers}%", fontdict={"fontsize":13})
+            title = f"eps:{eps}, ms:{min_samples},\nncluster:{ncluster}, mean_cls_size:{mean}, std_cls_size{std}, outliers:{outliers}"
+            plt.title(title, fontdict={"fontsize":13})
             plt.savefig(f"figs/{sido}/eps:{eps}_ms:{min_samples}.png")
             plt.close()
             progress_bar.update(1)

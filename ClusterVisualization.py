b3ce8454bd6f2384396ac24dce0aaf218ef4f59e
jhgan <jhgan00@gmail.com>
2020-06-16 21:20:17 +0900

    지도시각화 업데이트

--- ClusterVisualization.py
+++ ClusterVisualization.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 from folium import plugins
 from folium.map import FeatureGroup, LayerControl
 
+seoul = gpd.read_file("data/maps/seoul.geojson")
 schoolzone = gpd.read_file("data/maps/schoolzone-seoul.geojson")
 cluster = gpd.read_file("data/cluster.geojson")
 acdnt_cls = gpd.read_file("data/acdnt-cls.geojson")
@@ -12,10 +13,17 @@ featuregroup2 = FeatureGroup(name="어린이보호구역추천")
 featuregroup3 = FeatureGroup(name="스쿨존")
 
 
+def seoul_style(feature):
+    style = {
+        'fillColor': '#00000000'
+    }
+    return style
+
+
 def cluster_style(feature):
     style = {
-        "color": "red",
-        "fillcolor": "red",
+        'color': "red",
+        "fillColor": "red",
         "opacity": 0.9
     }
     return style
@@ -23,15 +31,16 @@ def cluster_style(feature):
 
 def schoolzone_style(feature):
     style = {
-        "color": "green",
-        "fillcolor": "green",
+        'color': "green",
+        'fillColor': "#D68910",
         "opacity": 0.6
     }
     return style
 
 
-m = folium.Map(location=[37.53, 126.97], zoom_start=12)
+m = folium.Map(location=[37.54, 126.98], zoom_start=12)
 
+folium.features.GeoJson(seoul, seoul_style, name="서울시 경계").add_to(m)
 folium.features.GeoJson(acdnt_cls).add_to(featuregroup1)
 folium.features.GeoJson(cluster, cluster_style).add_to(featuregroup2)
 folium.features.GeoJson(schoolzone, schoolzone_style).add_to(featuregroup3)
